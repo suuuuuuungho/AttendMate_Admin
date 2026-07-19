1f9dfff7a4b4b01c6b2e8946c3e93307f82eb8a6
suuuuuuungho <parksh5573@gmail.com>
2026-07-19 10:32:45 +0900

    Add 7/19(주) 중등부예배 time slot to match AttendMate

--- assets/js/config.js
+++ assets/js/config.js
@@ -4,6 +4,7 @@ export const SUPABASE_ANON_KEY = "sb_publishable_IXVkIRdwEmrEW9Bshsb5dw_okT8thEw
 
 // 좌석 배치/통계 페이지와 동일한 타임 목록 — Control Panel에서 이 목록 각각의 활성/비활성을 관리한다.
 export const TIMES = [
+  "7/19(주) 중등부예배",
   "7/27(월) 저녁",
   "7/28(화) 오전",
   "7/28(화) 저녁",
